ac74bf07eb493b74620a1ce973b564ea41f892c6
Gleb_Zharkov <Gleb_Zharkovrus@mail.ru>
2020-03-06 21:56:15 +0500

    Изменены переменные

--- Table_maslo.php
+++ Table_maslo.php
@@ -1,10 +1,10 @@
 <?php
 	$fh = fopen("D:/testo.txt", "r");
     list($f1,$f2,$f3)= fscanf($fh, "%s %s %s");
-	$host_xleb = "$f1";
+	$h_xleb = "$f1";
 	$db_name_xleb = "$f2";
-	$username_xleb = "$f3";
-	$password_xleb = "";
+	$u_xleb = "$f3";
+	$p_xleb = "";
 	// Подключение к базе данных
 	try {
 		$bd_xleb = new PDO("mysql:host={$host_xleb};dbname={$db_name_xleb};", $username_xleb, $password_xleb);
